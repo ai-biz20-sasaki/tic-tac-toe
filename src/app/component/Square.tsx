86b858a8a64050512ccf95d5701d1fb7fa48ec94
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-09 13:32:13 +0900

    clickイベントを追加

--- src/app/component/Square.tsx
+++ src/app/component/Square.tsx
@@ -1,9 +1,21 @@
+'use client';
+//親：Board->子：Square
 type SquareProps = {
   value: string
 }
 
 export default function Square(props: SquareProps) {
+
+  const handleclick = () => {
+    console.log('clicked!');
+  }
+
   return (
-    <button className="square">{props.value}</button>
+    <button
+      className="square"
+      onClick={handleclick}
+    >
+      {props.value}
+    </button>
   )
 }
