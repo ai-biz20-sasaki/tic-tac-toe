d86e312ad9b1668d5475d25db061603b8c7adf48
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-09 11:49:28 +0900

    Squareをcomponent化した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Square from "./component/Square";
 
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Home() {
+  return (
+    <Square />
+  )
 }
